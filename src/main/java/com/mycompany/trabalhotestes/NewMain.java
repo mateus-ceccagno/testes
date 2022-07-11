ebf6b25161d73e2b21d0a39e267d961baefe7ee7
mateus-ceccagno <sk.mateus@hotmail.com>
2022-07-10 21:44:04 -0300

    facade class for client

--- src/main/java/com/mycompany/trabalhotestes/NewMain.java
+++ src/main/java/com/mycompany/trabalhotestes/NewMain.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
  */
 public class NewMain {
     public static void main(String[] args) {
+        
+        // CÓDIGO SEM FACADE 
         /*
         IMC imc = new IMC();
         ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>() {
@@ -25,12 +27,23 @@ public class NewMain {
         
         for (Pessoa pessoa : pessoas) {
             float imcCalculado = imc.obterIMC(pessoa);
-            String classifcacao = imc.obterClassificacao(imcCalculado);
-            
-            System.out.print(pessoa.toString());
+            String classifcacaoIMC = imc.obterClassificacao(pessoa);
+            String classifcacaoEtaria = imc.obterClassifcacaoEtaria(pessoa.idade);
+            System.out.print(pessoa.toString() + obterClassifcacaoEtaria);
             System.out.print(" => ");
-            System.out.println("IMC: " + imcCalculado + ", " + classifcacao);
+            System.out.println("IMC: " + imcCalculado + ", " + classifcacaoIMC);
         }
         */
+        
+        // CÓDIGO COM FACADE 
+        FacadeIMC fachada = new FacadeIMC();        
+        ArrayList<PessoaRetorno>resultado = new ArrayList<PessoaRetorno>() {
+            {
+                add(fachada.gerarPessoaParaIMC("Mary", 40.3f, 1.70f, 22));
+                add(fachada.gerarPessoaParaIMC("John", 95.1f, 1.70f, 89));
+                add(fachada.gerarPessoaParaIMC("Peter", 95.1f, 1.90f, 36));
+                add(fachada.gerarPessoaParaIMC("Elizabeth", 105.1f, 1.70f, 36));
+            }
+        };
     }
 }
